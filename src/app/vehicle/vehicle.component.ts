27b1bc5c15546500198479cccd8336a8d194a28d
John Lee <jlee@openlane.com>
2017-04-11 18:38:06 -0700

    Add Some Angular

--- src/app/vehicle/vehicle.component.ts
+++ src/app/vehicle/vehicle.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+
+import {Vehicle} from "./vehicle";
+const CARS: Vehicle[] = [
+  { id: 11, content: 'Audi' },
+  { id: 12, content: 'Bugatti' },
+  { id: 13, content: 'Chev' },
+  { id: 14, content: 'Dodge' },
+];
+
 @Component({
   selector: 'vehicle-home',
   templateUrl: './vehicle.component.html',
 })
 export class VehicleComponent {
+  cars = CARS;
 }
